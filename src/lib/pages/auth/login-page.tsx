bc43c9664e564985d63cd389f1c8a78358d87f8a
blogstevens@gmail.com <blogstevens@gmail.com>
2024-07-12 00:53:39 +0700

    login ver 2

--- src/lib/pages/auth/login-page.tsx
+++ src/lib/pages/auth/login-page.tsx
@@ -4,8 +4,10 @@ import { FiLock } from "react-icons/fi";
 import { UserLogin } from "../../types/user-type";
 import { loginUser } from "../../services/user-service";
 import Loader from "../../../components/loader";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   const [userData, setUserData] = useState<UserLogin>({
     email: "",
     password: "",
@@ -14,14 +16,17 @@ export default function LoginPage() {
 
   const handleLogin = async () => {
     const response = await loginUser(userData);
+    if (response.success) {
+      navigate("/");
+    }
     console.log(response.message);
   };
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-primary">
-      <div className="w-full h-full flex justify-between items-center px-8 py-8">
-        <div className="w-[50%] h-full justify-center items-center p-10">
-          <div className="w-full h-full flex flex-col pl-10 pr-52 gap-3 justify-center text-white">
+      <div className="w-full h-full flex justify-center lg:justify-between items-center px-8 py-8">
+        <div className="w-96 lg:w-[50%] h-full justify-center items-center p-2 sm:p-10">
+          <div className="w-full h-full flex flex-col lg:pl-10 lg:pr-32 xl:pr-52 gap-3 justify-center text-white">
             <div className="text-2xl font-bold text-accent">Login</div>
             <div className="text-base text-gray-500">
               Hello! Enter your details to log in.
@@ -38,7 +43,7 @@ export default function LoginPage() {
                 }
               />
             </div>
-            <div className="w-full flex items-center bg-secondary rounded-3xl px-5 focus-within:border">
+            <div className="flex items-center bg-secondary rounded-3xl px-5 focus-within:border">
               <FiLock className="text-xl" />
               <input
                 className="py-4 pl-4 placeholder:text-sm w-full indent-3 text-sm bg-transparent focus-visible:outline-none"
@@ -51,20 +56,26 @@ export default function LoginPage() {
               />
             </div>
             {loading ? (
-              <button className="bg-neutral rounded-3xl text-accent font-bold p-3 flex justify-center items-center hover:cursor-default">
+              <button className=" bg-neutral rounded-3xl text-accent font-bold p-3 flex justify-center items-center hover:cursor-default">
                 <Loader />
               </button>
             ) : (
               <button
-                className="bg-neutral rounded-3xl text-accent font-bold p-3 hover:bg-accent hover:text-neutral transition-all duration-300 ease-in-out active:scale-95"
+                className=" bg-neutral rounded-3xl text-accent font-bold p-3 hover:bg-accent hover:text-neutral transition-all duration-300 ease-in-out active:scale-95"
                 onClick={handleLogin}
               >
                 Login
               </button>
             )}
+            <p className="mt-6 text-sm text-accent">
+              Don't have an account?{" "}
+              <span className="font-bold hover:cursor-pointer hover:text-neutral transition-all duration-300 ease-in-out">
+                Register
+              </span>
+            </p>
           </div>
         </div>
-        <div className="w-[50%] bg-gray-800 h-full rounded-lg"></div>
+        <div className="hidden lg:flex w-[50%] bg-gray-800 h-full rounded-lg"></div>
       </div>
     </div>
   );
