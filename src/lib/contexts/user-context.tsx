1a4e3d5f8f29fa87bac8489ea26244d82b97b04a
victorhalimm <victorhalim18@gmail.com>
2024-07-12 01:12:51 +0700

    fix context

--- src/lib/contexts/user-context.tsx
+++ src/lib/contexts/user-context.tsx
@@ -1,5 +1,4 @@
 
-import { doc, getDoc } from "firebase/firestore";
 import { createContext, useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import IChildren from "../pages/layout/IChildren";
@@ -11,32 +10,17 @@ export const UserContext = createContext<User | null>(null);
 export default function UserContextProvider({children} : IChildren) {
 
     const [user, setUser] = useState<User | null>(null);
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(async (currentUser : any) => {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+        // @ts-ignore
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, [location]);
 
-            if(currentUser) {
-                const docSnap = await getDoc(doc(db, "users", currentUser.uid));
-                if (docSnap.exists()) { 
-                    currentUser["email"] = docSnap.data()["email"];
-                    currentUser["age"] = docSnap.data()["age"];
-                    currentUser["gender"] = docSnap.data()["gender"];
-                    currentUser["role"] = docSnap.data()["role"];
-                }
-            }
-
-            //@ts-ignore
-            setUser(currentUser);
-        });
-        return () => unsubscribe();
-    }, [location])
-
-    return (
-        <UserContext.Provider value={user}>
-            {children}
-        </UserContext.Provider>
-    )
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 }
 
 export const useUser = () => {
