a5a7fe710a0070f62a2e0648d35f23565b45f0a0
lc093 <ryan.rafael@binus.ac.id>
2024-07-12 10:13:05 +0700

    added route to detail page

--- src/lib/routes/route.tsx
+++ src/lib/routes/route.tsx
@@ -5,6 +5,7 @@ import LoginPage from "../pages/auth/login-page";
 import RegisterPage from "../pages/auth/register-page";
 import HomePage from "../pages/home/home-page";
 import TestPage from "../pages/backend-test/test-page";
+import PostDetailPage from "../pages/post/post-detail-page";
 
 export const router = [
   {
@@ -30,5 +31,9 @@ export const router = [
   {
     path: "/test",
     element: <TestPage />
+  },
+  {
+    path: "/post/:id",
+    element: <PostDetailPage />
   }
 ];
